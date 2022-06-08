9b7dc41d046903b37e7321e623b5eca53d31c558
Matthew Barber <quitesimplymatt@gmail.com>
2022-06-08 16:09:01 +0100

    Rudimentary generation of dataframes with `hypopthesis`

--- test_interchange.py
+++ test_interchange.py
@@ -1,14 +1,14 @@
 from typing import Any, Callable, Dict, NamedTuple
 
 import pytest
-
-example_dict = {"n": [42]}
+from hypothesis import assume, given
+from hypothesis import strategies as st
 
 DataFrame = Any
 
 
 class LibraryInfo(NamedTuple):
-    example: DataFrame
+    strategy: st.SearchStrategy[DataFrame]
     from_dataframe: Callable[[DataFrame], DataFrame]
     frame_equal: Callable[[DataFrame, DataFrame], bool]
 
@@ -17,13 +17,35 @@ lib_params: list = []
 lib_to_linfo: Dict[str, LibraryInfo] = {}
 
 try:
-    import pandas
+    import numpy as np
+    import pandas as pd
+    from hypothesis.extra import pandas as pds
     from pandas.api.exchange import from_dataframe as pandas_from_dataframe
 except ImportError as e:
     lib_params.append(pytest.param("pandas", marks=pytest.mark.skip(reason=e.msg)))
 else:
+    valid_dtypes = [np.bool_]  # TODO: str, datetimes, categories
+    for kind in ["int", "uint"]:
+        for bitwidth in [8, 16, 32, 64]:
+            valid_dtypes.append(np.dtype(f"{kind}{bitwidth}"))
+    for bitwidth in [32, 64]:
+        valid_dtypes.append(np.dtype(f"float{bitwidth}"))
+
+    @st.composite
+    def dataframes(draw) -> st.SearchStrategy[pd.DataFrame]:
+        colnames_strat = st.from_regex("[a-z]+", fullmatch=True)
+        colnames = draw(st.lists(colnames_strat, min_size=1, unique=True))
+        columns = []
+        for colname in colnames:
+            dtype = draw(st.sampled_from(valid_dtypes))
+            column = pds.column(colname, dtype=dtype)
+            columns.append(column)
+        df = draw(pds.data_frames(columns))
+        assume(df.shape[0] != 0)  # TODO: generate empty dataframes
+        return df
+
     linfo = LibraryInfo(
-        example=pandas.DataFrame(example_dict),
+        strategy=dataframes(),
         from_dataframe=pandas_from_dataframe,
         frame_equal=lambda df1, df2: df1.equals(df2),
     )
@@ -52,7 +74,7 @@ else:
         return True
 
     linfo = LibraryInfo(
-        example=vaex.from_dict(example_dict),
+        strategy=st.just(vaex.from_dict({"n": [42]})),
         from_dataframe=vaex_from_dataframe,
         frame_equal=vaex_frame_equal,
     )
@@ -69,7 +91,7 @@ except ImportError as e:
 else:
     Engine.put("ray")
     linfo = LibraryInfo(
-        example=modin.pandas.DataFrame(example_dict),
+        strategy=st.just(modin.pandas.DataFrame({"n": [42]})),
         from_dataframe=modin_from_dataframe,
         frame_equal=lambda df1, df2: df1.equals(df2),
     )
@@ -80,14 +102,15 @@ else:
 # parametrize order is intentional for sensical pytest param ids
 @pytest.mark.parametrize("dest_lib", lib_params)
 @pytest.mark.parametrize("orig_lib", lib_params)
-def test_from_dataframe_roundtrip(orig_lib: str, dest_lib: str):
+@given(data=st.data())
+def test_from_dataframe_roundtrip(orig_lib: str, dest_lib: str, data: st.DataObject):
     """
     Round trip of dataframe interchange results in a dataframe identical to the
     original dataframe.
     """
     orig_linfo = lib_to_linfo[orig_lib]
     dest_linfo = lib_to_linfo[dest_lib]
-    orig_df = orig_linfo.example
+    orig_df = data.draw(orig_linfo.strategy, label="df")
     dest_df = dest_linfo.from_dataframe(orig_df)
     roundtrip_df = orig_linfo.from_dataframe(dest_df)
     assert orig_linfo.frame_equal(roundtrip_df, orig_df), (
