ee5107de72e3c58b51fb2c4a7ab6ac8c59f07d73
Matthew Barber <quitesimplymatt@gmail.com>
2022-06-08 13:15:39 +0100

    Assert message clean-up

--- test_interchange.py
+++ test_interchange.py
@@ -91,17 +91,11 @@ def test_from_dataframe_roundtrip(orig_lib: str, dest_lib: str):
     dest_df = dest_linfo.from_dataframe(orig_df)
     roundtrip_df = orig_linfo.from_dataframe(dest_df)
     assert orig_linfo.frame_equal(roundtrip_df, orig_df), (
-        f"Round trip of dataframe did not result in an identical dataframe.\n"
-        "\n"
-        f"Original dataframe ({orig_lib}):\n"
-        "\n"
-        f"{orig_df}\n"
-        "\n"
-        f"Intermediate dataframe ({dest_lib}):\n"
-        "\n"
-        f"{dest_df}\n"
-        "\n"
-        f"Round trip dataframe ({orig_lib}):\n"
-        "\n"
+        f"Round trip of dataframe did not result in an identical dataframe.\n\n"
+        f"Original dataframe ({orig_lib}):\n\n"
+        f"{orig_df}\n\n"
+        f"Intermediate dataframe ({dest_lib}):\n\n"
+        f"{dest_df}\n\n"
+        f"Round trip dataframe ({orig_lib}):\n\n"
         f"{roundtrip_df}\n"
     )
