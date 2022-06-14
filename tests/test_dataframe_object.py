2cb6b8692c374be3f8ea077aa2ee07b421d97ba0
Matthew Barber <quitesimplymatt@gmail.com>
2022-06-14 15:51:41 +0100

    Test `__dataframe__()` for compliant dfs

--- tests/test_dataframe_object.py
+++ tests/test_dataframe_object.py
@@ -6,12 +6,22 @@ from hypothesis import strategies as st
 from .wrappers import LibraryInfo
 
 
-@given(data=st.data())
-def test_toplevel_dunder_dataframe(linfo: LibraryInfo, data: st.DataObject):
-    df = data.draw(linfo.toplevel_strategy, label="df")
+def _test_dunder_dataframe(df):
     assert hasattr(df, "__dataframe__")
     assert isinstance(df.__dataframe__, Callable)
     out = df.__dataframe__()
     assert isinstance(out, dict)
     assert hasattr(out, "dataframe")
     assert hasattr(out, "version")
+
+
+@given(data=st.data())
+def test_toplevel_dunder_dataframe(linfo: LibraryInfo, data: st.DataObject):
+    df = data.draw(linfo.toplevel_strategy, label="df")
+    _test_dunder_dataframe(df)
+
+
+@given(data=st.data())
+def test_dunder_dataframe(linfo: LibraryInfo, data: st.DataObject):
+    df = data.draw(linfo.compliant_strategy, label="df")
+    _test_dunder_dataframe(df)
