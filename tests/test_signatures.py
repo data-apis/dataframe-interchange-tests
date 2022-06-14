1a03138854e20bfd92d12d43bddb0eb86c4b9718
Matthew Barber <quitesimplymatt@gmail.com>
2022-06-14 10:36:33 +0100

    Test column method signatures

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -3,10 +3,10 @@ from types import FunctionType
 from typing import Callable
 
 import pytest
-from hypothesis import given, settings
+from hypothesis import assume, given, note, settings
 from hypothesis import strategies as st
 
-from .api import DataFrame
+from .api import Column, DataFrame
 from .wrappers import LibraryInfo
 
 VAR_KINDS = (Parameter.VAR_POSITIONAL, Parameter.VAR_KEYWORD)
@@ -23,18 +23,18 @@ def _test_signature(func, stub):
     sig = signature(func)
     stub_sig = signature(stub)
     params = list(sig.parameters.values())
-    stub_params = list(stub_sig.parameters.values())
+    df_stub_params = list(stub_sig.parameters.values())
     # sanity checks
     if len(params) > 0:
         assert params[0].name != "self"
-    assert stub_params[0].name == "self"
-    del stub_params[0]
+    assert df_stub_params[0].name == "self"
+    del df_stub_params[0]
 
     non_kwonly_stub_params = [
-        p for p in stub_params if p.kind != Parameter.KEYWORD_ONLY
+        p for p in df_stub_params if p.kind != Parameter.KEYWORD_ONLY
     ]
     # sanity check
-    assert non_kwonly_stub_params == stub_params[: len(non_kwonly_stub_params)]
+    assert non_kwonly_stub_params == df_stub_params[: len(non_kwonly_stub_params)]
     # We're not interested if the array module has additional arguments, so we
     # only iterate through the arguments listed in the spec.
     for i, stub_param in enumerate(non_kwonly_stub_params):
@@ -56,7 +56,7 @@ def _test_signature(func, stub):
                 f"but should be a {f_stub_kind}"
             )
 
-    kwonly_stub_params = stub_params[len(non_kwonly_stub_params) :]
+    kwonly_stub_params = df_stub_params[len(non_kwonly_stub_params) :]
     for stub_param in kwonly_stub_params:
         assert (
             stub_param.name in sig.parameters.keys()
@@ -72,27 +72,45 @@ def _test_signature(func, stub):
 
 @given(data=st.data())
 @settings(max_examples=1)
-def test_top_level_dataframe_dunder_dataframe(linfo: LibraryInfo, data: st.DataObject):
+def test_toplevel_dataframe_dunder_dataframe(linfo: LibraryInfo, data: st.DataObject):
     df = data.draw(linfo.toplevel_strategy, label="df")
     assert hasattr(df, "__dataframe__")
     assert isinstance(df.__dataframe__, Callable)
     _test_signature(df.__dataframe__, DataFrame.__dataframe__)
 
 
-stub_params = []
+df_stub_params = []
 for _, stub in getmembers(DataFrame, predicate=isfunction):
     p = pytest.param(stub, id=stub.__name__)
-    stub_params.append(p)
+    df_stub_params.append(p)
 
 
-@pytest.mark.parametrize("stub", stub_params)
+@pytest.mark.parametrize("stub", df_stub_params)
 @given(data=st.data())
 @settings(max_examples=1)
-def test_compliant_dataframe_method(
-    linfo: LibraryInfo, stub: FunctionType, data: st.DataObject
-):
+def test_dataframe_method(linfo: LibraryInfo, stub: FunctionType, data: st.DataObject):
     df = data.draw(linfo.compliant_strategy, label="df")
     assert hasattr(df, stub.__name__)
     method = getattr(df, stub.__name__)
+    assert isinstance(method, Callable)
+    _test_signature(method, stub)
+
+
+col_stub_params = []
+for _, stub in getmembers(Column, predicate=isfunction):
+    p = pytest.param(stub, id=stub.__name__)
+    col_stub_params.append(p)
+
+
+@pytest.mark.parametrize("stub", col_stub_params)
+@given(data=st.data())
+@settings(max_examples=1)
+def test_column_method(linfo: LibraryInfo, stub: FunctionType, data: st.DataObject):
+    df = data.draw(linfo.compliant_strategy, label="df")
+    assume(df.num_columns() > 0)
+    col = df.get_column(0)
+    note(f"{col=}")
+    assert hasattr(col, stub.__name__)
+    method = getattr(col, stub.__name__)
     assert isinstance(method, Callable)
     _test_signature(method, stub)
